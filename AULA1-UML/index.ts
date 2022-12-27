15abc7b55c524e861ebdbb1c0a64f7802f7664af
phacf <phacff@gmail.com>
2022-12-27 18:31:11 -0300

    builder example

--- AULA1-UML/index.ts
+++ AULA1-UML/index.ts
@@ -1,35 +1,33 @@
 //UML
-export class UmaClasse{
-    constructor(
-        private name: string,
-        public sobrenome: string,
-        protected idade: number
-    ){}
+export class UmaClasse {
+  constructor (
+    private name: string,
+    public sobrenome: string,
+    protected idade: number
+  ) {}
 
-    public getName(){
-        return this.name;
-    }
+  public getName () {
+    return this.name;
+  }
 
-    public getIdade(){
-        return this.idade;
-    }
+  public getIdade () {
+    return this.idade;
+  }
 
-    public setNome(name: string){
-        this.name = name;
-    }
+  public setNome (name: string) {
+    this.name = name;
+  }
 
-    public setIdade(age: number){
-        this.idade = age;
-    }
+  public setIdade (age: number) {
+    this.idade = age;
+  }
 }
 
 //herança
-export class Shape {
+export class Shape {}
 
-}
-
-export class Circle extends Shape{
-    // herda todos as caracteristicas de shape
+export class Circle extends Shape {
+  // herda todos as caracteristicas de shape
 }
 
 //agregação e composição
@@ -37,84 +35,73 @@ export class Circle extends Shape{
 export class Motor {}
 //agregação
 export class Carro {
-    private motor: any;
+  private motor: any;
 
-    constructor(motor: any){
-        this.motor = motor
-    }
+  constructor (motor: any) {
+    this.motor = motor;
+  }
 }
-const motor = new Motor()
+const motor = new Motor();
 //composição
-const carro = new Carro(motor)
-
+const carro = new Carro(motor);
 
 //dependencia
 /* quando uma classe referencia outra classe dentro do seu corpo*/
 
-export class Printer{
-    public print( document: any){
-        console.log(`printing ${document}`)
-    }
+export class Printer {
+  public print (document: any) {
+    console.log(`printing ${document}`);
+  }
 }
 
-export class Document{
-
-}
+export class Document {}
 
 //REALIZATION
 
 /*quando uma classe implementa uma interface ou uma interface extendendo outra
 interface é um contrato para que outras classes implementem seu conteudo*/
 
-export interface ControleRemoto{
-    play(): void;
-    pause(): void;
+export interface ControleRemoto {
+  play(): void;
+  pause(): void;
 }
 
 export interface ControleSom extends ControleRemoto {
-    mudarRadio(): void;
+  mudarRadio(): void;
 }
 
-export class Dvd implements ControleRemoto{
-    pause(): void {
-        //set pause
-    }
-    play(): void {
-        //set play
-    }
+export class Dvd implements ControleRemoto {
+  pause (): void {
+    //set pause
+  }
+  play (): void {
+    //set play
+  }
 }
 
-export class Radio implements ControleSom{
-    play(): void {
-        
-    }
-    pause(): void {
-        
-    }
-    mudarRadio(): void {
-        
-    }
+export class Radio implements ControleSom {
+  play (): void {}
+  pause (): void {}
+  mudarRadio (): void {}
 }
-
 
 //abstração
 
 //toda classe que extender animal tem que criar makeNoise é uma forma de interface mais complexa
 
-
 export abstract class Animal {
-    protected abstract makeNoise(): string;
+  protected abstract makeNoise (): string;
 
-    makeSound(): void {
-        console.log(this.makeNoise())
-    }
+  makeSound (): void {
+    console.log(this.makeNoise());
+  }
 }
 
 export class Dog extends Animal {
-    protected makeNoise(): string {
-        return 'au au'
-    }
+  protected makeNoise (): string {
+    return 'au au';
+  }
 }
 
-const dog =  new Dog();
+const dog = new Dog();
 dog.makeSound();
---
+++ AULA3-builder/index.ts
@@ -0,0 +1,10 @@
+/****
+ * visa separar construção de objeto complexo do codigo do cliente para manter codigo mais limpo
+ * composição de varios objetos (composite)
+ * algoritmo de criação do objeto complexo
+ * permite criar objeto em etapas
+ * permite method chaining
+ * objeto final varia de acordo com a necessidade
+ * ****/
+
+abstract class BuilderInterface {}
